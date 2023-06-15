3e13177d250cf1efe0390862fbf15bd91cc455c7
Febin <febinj14@gmail.com>
2023-06-15 15:25:21 +0530

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+const TodoItem = ({ completed, id, title, toggleTodo, handleDelete }) => {
+
+  return (
+    <>
+      <li>
+        <label>
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={(e) => toggleTodo(id, e.target.checked)}
+          />
+          {title}
+        </label>
+        <button onClick={() => handleDelete(id)} className="btn btn-danger">
+          Delete
+        </button>
+      </li>
+    </>
+  );
+};
+
+export default TodoItem;
